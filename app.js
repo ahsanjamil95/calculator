ae553fd70d86627f87a9b5da3284130a3bbbf36c
ahsan <ahsanjamil@live.com>
2020-06-29 20:34:47 +0500

    calculator

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
 // JavaScript Document
 function numberGet(num) {
-  var display = document.getElementById('display');
-	display.value += num;
+  var cal_display = document.getElementById('cal_display');
+	cal_display.value += num;
 }
 function reset() {
-  var display = document.getElementById('display');
-	display.value = ""
+  var cal_display = document.getElementById('cal_display');
+	cal_display.value = ""
 }
 
 function result() {
-  var display = document.getElementById('display');
-	display.value = eval(display.value);
-}
-function backspace(){
-var display = document.getElementById('display');
-	display.value = display.value.slice(0,-1);
+  var cal_display = document.getElementById('cal_display');
+	cal_display.value = eval(cal_display.value);
 }
